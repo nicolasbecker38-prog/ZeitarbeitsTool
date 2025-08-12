78d91f5ad54b975f7717ee048075dc3ec9188066
nicolasbecker38-prog <nicolasbecker38@gmail.com>
2025-08-12 15:03:08 +0200

    Create package.js

---
+++ backend/package.js
@@ -0,0 +1,15 @@
+// backend/package.json
+{
+  "name": "zeitarbeit-backend",
+  "version": "1.0.0",
+  "description": "Backend für Zeitarbeit Vertriebs-Tool",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "nodemailer": "^6.9.4",
+    "cors": "^2.8.5"
+  }
+}
